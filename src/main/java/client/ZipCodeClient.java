84a75b1dfd647462ee6c6758ea4dd09e958f48f4
alexandrucarata <alexandrucarata@gmail.com>
2023-03-09 14:34:41 +0200

    switched to rest-assured

--- src/main/java/client/ZipCodeClient.java
+++ src/main/java/client/ZipCodeClient.java
@@ -1,12 +1,11 @@
 package client;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.javafaker.Faker;
-import data.Response;
+import httpclient.Scope;
 import io.qameta.allure.Step;
-import org.apache.http.HttpResponse;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -14,44 +13,36 @@ import java.util.stream.Collectors;
 
 import static data.Constants.ZIPCODES_EXPAND_RESOURCE;
 import static data.Constants.ZIPCODES_RESOURCE;
+import static io.restassured.RestAssured.given;
 
 public class ZipCodeClient {
     @Step("Get Zip Codes List")
-    public Response<List<String>> getZipCodesList() throws IOException {
-        Response<List<String>> response = new Response<>();
-        HttpResponse httpResponse = executeGetZipCodes();
-        response.setBody(getResponseAsList(httpResponse));
-        response.setStatusCode(httpResponse.getStatusLine().getStatusCode());
-        return response;
+    public Response getZipCodesList() {
+        return given()
+                .header("Authorization", "Bearer " + Authentication.getToken(Scope.READ))
+                .when()
+                .get(ZIPCODES_RESOURCE);
     }
 
     @Step("Add Zip Codes to Zip Codes List")
-    public Response<List<String>> addToZipCodesList(List<String> zipCodesList) throws IOException {
-        Response<List<String>> response = new Response<>();
-        HttpResponse httpResponse = executePostZipCodes(zipCodesList.toString());
-        response.setBody(getResponseAsList(httpResponse));
-        response.setStatusCode(httpResponse.getStatusLine().getStatusCode());
-        return response;
+    public Response addToZipCodesList(List<String> zipCodesList) {
+        return given()
+                .header("Authorization", "Bearer " + Authentication.getToken(Scope.WRITE))
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(zipCodesList)
+                .when()
+                .post(ZIPCODES_EXPAND_RESOURCE);
     }
 
     @Step("Get Newly Created Zip Code")
-    public String getNewZipCode() throws IOException {
+    public String getNewZipCode() {
         String newZipCode = new Faker().number().digits(5);
         addToZipCodesList(Collections.singletonList(newZipCode));
         return newZipCode;
     }
 
-    private static HttpResponse executeGetZipCodes() throws IOException {
-        return Client.executeGet(ZIPCODES_RESOURCE);
-    }
-
-    private static HttpResponse executePostZipCodes(String requestBody) throws IOException {
-        return Client.executePost(ZIPCODES_EXPAND_RESOURCE, requestBody);
-    }
-
-    private static List<String> getResponseAsList(HttpResponse response) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        return Arrays.stream(mapper.readValue(response.getEntity().getContent(), String[].class))
-                .collect(Collectors.toList());
+    public List<String> getResponseAsList(Response response) {
+        return Arrays.stream(response.getBody().as(String[].class)).collect(Collectors.toList());
     }
 }
